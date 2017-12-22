b106a476a3399a1cf1de3a027f0bdf4ce1991c3f
konstantin0s <constantintofan85@gmail.com>
2017-12-22 14:16:58 +0100

    codebreaker code

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -2,89 +2,63 @@ class Codebreaker
   class Game
     attr_reader :output
 
+
     def initialize(output)
       @output = output
+      @add_plus = 0
+      @add_minus = 0
     end
 
     def start(secret_number)
       @secret_number = secret_number
-
+      @unknown = @secret_number.split("").map { |number| number.to_i }
       output.puts "Welcome to Codebreaker"
       output.puts "Enter guess:"
     end
 
+    def print_minus
+      "-" * @add_minus
+    end
+
+    def print_plus
+      "+" * @add_plus
+    end
+
+
 
     def guess(input)
-      @input = input
-
-
-
-
-      if @secret_number.length != @input.length
-               output.puts "Try guessing a number with four digits"
-             elsif @secret_number.empty? && @input.empty?
-             else
-             end
-
-
-
-if @input[0] == @secret_number[1]
-   output.puts "-"
-elsif @secret_number[0] == @input[0]
-   output.puts "+"
-end
-
-
-
-if @secret_number[1][2] == @input[0][1]
-   output.puts "--"
-end
-if @secret_number[1][2] == @input[0][2]
-   output.puts "+-"
-end
-
-   @secret_number.each_char.with_index do | key, c |
-      if input.include?(key) && c == input.index(key)
-   output.puts "+-"
-      end
-   end
-
-
-str = []
-  @secret_number.each_char.with_index do |x, y|
-      if input.include?(x) && y == input.index(x)
-          str  << "+"
+      guessing = input.split("").map { |num| num.to_i }
+      if @secret_number.length != input.length
+        output.puts "Try guessing a number with four digits"
       else
-          str  << "+"
+        for exact in 0..3
+          @add_plus += 1 if guessing[exact] == @unknown[exact]
+        end
+        for exact in 0..3
+          for count in 0..3
+            if guessing[count] == @unknown[exact]
+              @add_minus += 1
+              guessing[count]= -1
+              break
+            end
+          end
+        end
+
+
+          @add_minus -= @add_plus
+
+        if @add_minus == 0 && @add_plus == 0
+          spit = ''
+        elsif @add_plus == 4
+          spit = print_plus
+        elsif @add_minus == 0
+          spit = print_plus if @add_plus != 0
+        elsif @add_plus == 0
+          spit = print_minus if @add_minus != 0
+
+        end
+        output.puts(spit)
       end
-         output.puts str.join("")
-     end
-
-
-array = []
-  @secret_number.each_char.with_index do |minus, zt|
-    if input.include?(minus) || zt == input.index(minus)
-       array << "-"
-    end
-       output.puts array.join("")
-    end
-
-put = []
-  @secret_number.each_char.with_index do |to ,mh|
-    if input.include?(to) && mh == input.index(to)
-      put << "++-"
-    end
-     output.puts put.join("")
-    end
-
-sav = []
-  @secret_number.each_char.with_index do |ug ,ik|
-    if input.include?(ug) && ik == input.index(ug)
-       sav << "++--"
-    end
-       output.puts put.join("")
-    end
-
     end
   end
 end
